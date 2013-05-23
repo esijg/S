c35eac45dff2204d38e137b75f82801247fb8357
Kyle Halladay <k.mj.halladay@gmail.com>
2013-05-22 23:31:50 -0400

    Island Puzzle works, rough looking, but it works

--- S/Assets/Scripts/CylinderPuzzlePiece.cs
+++ S/Assets/Scripts/CylinderPuzzlePiece.cs
@@ -2,23 +2,46 @@ using UnityEngine;
 using System.Collections;
 
 public class CylinderPuzzlePiece : MonoBehaviour {
-
+	
+	static int numActivated = 0;
+	
 	public Color onColor;
 	bool activated = false;
 	float time = 0.0f;
 	public CylinderPuzzle parentPuzzle;
+	public Material sourceMaterial;
+	public Renderer[] materialObjects;
+	Material ourMaterial;
+	public string name = "";
+	
+	public LightningBolt streamEmitter;
+	
+	void Start()
+	{
+		ourMaterial = new Material(sourceMaterial);
+		ourMaterial.name = ""+name+"(instance)";
+		foreach(Renderer r in materialObjects) r.material = ourMaterial;
+	}
 	
 	void Update()
 	{
-		this.renderer.material.color = Color.Lerp(renderer.material.color, onColor,( Time.time-time)/1.5f);
+		if (activated) ourMaterial.color = Color.Lerp(Color.black, onColor,( Time.time-time)/1.5f);
 	}
 	
-	void OnCollisionEnter(Collision collision)
+	void OnTriggerEnter(Collider collision)
 	{
+		Debug.Log("HIT");
 		if (collision.gameObject.tag == "SoundCube")
 		{
-			if (!activated)
+			if (!activated)	
 			{
+				numActivated++;
+				if (numActivated == 4)
+				{
+					GameObject.Find("Stream 2").GetComponent<ParticleRenderer>().enabled = true;
+				}
+				Debug.Log("Activated");
+				streamEmitter.enabled = true;
 				activated = true;
 				time = Time.time;
 				parentPuzzle.PieceActivated();
