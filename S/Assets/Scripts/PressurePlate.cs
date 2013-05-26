10c4e24bb4b2983e7fd138e72ab20e90c9e8a42a
Kyle Halladay <k.mj.halladay@gmail.com>
2013-05-26 10:32:51 -0400

    pressure plate puzzle looks a bit better

--- S/Assets/Scripts/PressurePlate.cs
+++ S/Assets/Scripts/PressurePlate.cs
@@ -6,10 +6,14 @@ public class PressurePlate : MonoBehaviour {
 	
 	public float neededWeight = 10.0f;
 	public float currentWeight = 0.0f;
-	public ParticleEmitter activatedSystem;
+	public ParticleRenderer activatedSystem;
 	Vector3 onPosition, offPosition;
 	bool activated = false;
-	
+	public AudioSource onAudio;
+	public Color onColor;
+	Color offColor;
+	Material instancedMaterial;
+	public Light onLight;
 	float activationTime = 0.0f;
 	
 	// Use this for initialization
@@ -17,14 +21,25 @@ public class PressurePlate : MonoBehaviour {
 	{
 		offPosition = transform.position;
 		onPosition = transform.position - transform.up*0.3f;
+		instancedMaterial = new Material(renderer.material);
+		instancedMaterial.name = renderer.material.name+"(instanced)";
+		offColor = instancedMaterial.color;
+		renderer.material = instancedMaterial;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-		if (activated)
+		float weight = (currentWeight > neededWeight)?neededWeight:currentWeight;
+		transform.position = Vector3.MoveTowards(transform.position, offPosition-transform.up* ( weight/neededWeight * 0.3f),0.01f);
+		
+		if (currentWeight >=neededWeight)
 		{
-			transform.position = Vector3.Lerp(offPosition, onPosition, (Time.time - activationTime)/1.0f);
+			renderer.material.color = Color.Lerp(offColor, onColor, (Time.time - activationTime)/1.0f);
+		}
+		else 
+		{
+			renderer.material.color = Color.Lerp(onColor, offColor, (Time.time - activationTime)/1.0f);
 		}
 		
 	}
@@ -44,6 +59,9 @@ public class PressurePlate : MonoBehaviour {
 					activatedSystem.enabled = true;
 					activationTime = Time.time;
 					activated = true;
+					onLight.enabled = true;
+					onAudio.enabled = true;
+					onAudio.Play();
 				}
 			}
 		}
@@ -54,6 +72,15 @@ public class PressurePlate : MonoBehaviour {
 		if (collision.gameObject.tag == "SoundCube")
 		{
 			currentWeight-=collision.gameObject.transform.localScale.magnitude;
+			if (currentWeight < neededWeight)
+			{
+				activationTime = Time.time;
+				activatedSystem.enabled = false;
+				activated = false;
+				WorldState.streamsSolved--;
+				onLight.enabled = false;
+				onAudio.enabled = false;
+			}
 		
 		}
 		
