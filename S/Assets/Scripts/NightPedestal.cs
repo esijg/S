ced98665f8ddaeeed5ce5717e906831880a6cc1b
Kyle Halladay <k.mj.halladay@gmail.com>
2013-05-26 17:20:17 -0400

    more ui stuff

--- S/Assets/Scripts/NightPedestal.cs
+++ S/Assets/Scripts/NightPedestal.cs
@@ -12,6 +12,12 @@ public class NightPedestal : MonoBehaviour {
 	Color lightColor;
 	Color camColor;
 	Color fogColor;
+	public LightningBolt nightBolt;
+	public ParticleRenderer[] sideBolts;
+	int iter = 0;
+	bool occupied = false;
+	
+	public Material nightMaterial;
 	// Use this for initialization
 	void Start () {
 		time = Time.time;
@@ -23,13 +29,35 @@ public class NightPedestal : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
+		
+		if (occupied && !activated)
+		{
+			if (cube.GetComponent<SoundCubeID>().thrown)
+			{
+				Debug.Log("cube dropped");
+				time = Time.time;
+				activated = true;
+				moon.SetActive(true);
+				cube.rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+				cube.collider.enabled = false;
+				cube.rigidbody.velocity = Vector3.zero;
+				cube.transform.parent = this.transform;
+				cube.transform.localPosition = Vector3.zero;
+				cube.rigidbody.useGravity = false;
+				cube.GetComponent<Buoyancy>().enabled = false;
+				Invoke("KillFog", 1.0f);
+				nightBolt.enabled = true;
+			}
+			
+		}
+		
 		if (activated)
 		{
 			Camera.mainCamera.backgroundColor = Color.Lerp(camColor, Color.black, (Time.time - time)/animDuration);
 			sun.intensity-= 0.001f;
 			RenderSettings.ambientLight = Color.Lerp(lightColor, Color.black, (Time.time - time)/animDuration);
 			RenderSettings.fogColor = Color.Lerp(fogColor, Color.clear, (Time.time - time)/animDuration);
-			cube.transform.localPosition = Vector3.Lerp(cube.transform.position, new Vector3(0.009505763f, 0.1321318f, -0.05523976f), Time.deltaTime);
+			cube.transform.localPosition = Vector3.Lerp(cube.transform.localPosition, new Vector3(0.009505763f, 0.1321318f, -0.05523976f), Time.deltaTime);
 		}
 	}
 	
@@ -37,18 +65,42 @@ public class NightPedestal : MonoBehaviour {
 	{
 		if (other.gameObject.tag == "SoundCube")
 		{
-			time = Time.time;
-			activated = true;
-			moon.SetActive(true);
-			other.gameObject.rigidbody.velocity = Vector3.zero;
-			other.gameObject.transform.parent = this.transform;
+			occupied = true;
 			cube = other.gameObject;
-			Invoke("KillFog", animDuration);
+
+			if (other.gameObject.GetComponent<SoundCubeID>().thrown){
+				Debug.Log("cube dropped");
+				time = Time.time;
+				activated = true;
+				moon.SetActive(true);
+				cube.GetComponent<Buoyancy>().enabled = false;
+				cube.rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+				cube.collider.enabled = false;
+				cube.rigidbody.velocity = Vector3.zero;
+				cube.transform.parent = this.transform;
+				cube.transform.localPosition = Vector3.zero;
+				cube.rigidbody.useGravity = false;
+				Invoke("KillFog", 1.0f);
+				nightBolt.enabled = true;
+			}
+		}
+	}
+	
+	void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.tag == "SoundCube")
+		{
+			occupied = false;
 		}
 	}
 	
 	void KillFog()
 	{
-			RenderSettings.fog = false;
+		if (iter < sideBolts.Length)
+		{
+			sideBolts[iter++].material = nightMaterial;
+			Invoke("KillFog", 1.0f);
+		}
+		else RenderSettings.fog = false;
 	}
 }
---
+++ S/Assets/Scripts/UI.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+
+public class UI : MonoBehaviour {
+	
+	public UISprite viewControls;
+	public UISprite controls;
+	public UISprite hideControls;
+	public UISprite logo;
+	public UISprite invertedLogo;
+	bool showing = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.C))
+		{
+			if ( !showing)
+			{
+				showing = true;
+				controls.enabled = true;
+				viewControls.enabled = false;
+				hideControls.enabled = true;
+			}
+			else
+			{
+				showing = false;
+				controls.enabled = false;
+				viewControls.enabled = false;
+				hideControls.enabled = false;
+			}
+		}
+	}
+}
